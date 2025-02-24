107ee10a0da7f4a1ebfeca80295a3fa67968d679
pestovakseniia <155980563+pestovakseniia@users.noreply.github.com>
2025-02-24 15:04:06 +0400

    An updated version of additional task for module 1

--- scripts for additional tasks/module_1_additional.py
+++ scripts for additional tasks/module_1_additional.py
@@ -22,15 +22,28 @@ def where_is_flat(floors, flats, user_flat):
         flat_in_floor = math.ceil((user_flat - (flat_in_building - 1) * floors * flats) / flats)
     return flat_in_building, flat_in_floor
 
-# Main execution block
-if __name__ == '__main__':
-    # User input for flat number
-    requested_flat = int(input("Input flat #: "))
-
-    # Validate user input; raise an error if the flat number is out of range
-    if requested_flat <= 0 or requested_flat > (BUILDINGS * FLOORS * FLATS):
+# Function to check user input: must be inside accepted range only
+def check_user_input(flat_number):
+    if not (1 <= flat_number <= BUILDINGS * FLOORS * FLATS):
         raise InvalidFlatNumberError()
 
-    # If the flat number is valid, calculate the building and floor
-    building_for_requested_flat, floor_for_requested_flat = where_is_flat(FLOORS, FLATS, requested_flat)
-    print(f"building: {building_for_requested_flat}, floor: {floor_for_requested_flat}")
+# Main execution block
+if __name__ == '__main__':
+    # Validate user input; raise an error if the flat number is out of range
+    # Iterate over until user inputs a correct flat number
+    while True:
+        # Get the user input and validate it
+        try:
+            requested_flat = int(input("Input flat #: "))
+            check_user_input(requested_flat)
+        # If user input is out of scope (i.e. 1 <= flat_number <= 80), handle an error
+        except InvalidFlatNumberError:
+            print("You entered the flat number out of accepted range. Try again")
+        # If user input a non-integer value, handle an error
+        except ValueError:
+            print("You entered the flat number out of accepted range. Try again")
+        # If the flat number is valid, calculate the building and floor
+        else:
+            building_for_requested_flat, floor_for_requested_flat = where_is_flat(FLOORS, FLATS, requested_flat)
+            print(f"building: {building_for_requested_flat}, floor: {floor_for_requested_flat}")
+            break
