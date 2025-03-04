95533c1a7e7386f75f57d03ed77128171ce9b1c0
pestovakseniia <155980563+pestovakseniia@users.noreply.github.com>
2025-03-04 18:45:33 +0400

    Fixed version of Module_3 additional task

--- scripts for additional tasks/Module_3_additional.py
+++ scripts for additional tasks/Module_3_additional.py
@@ -5,16 +5,18 @@ from datetime import datetime
 API_KEY = 'fca_live_RyexoUQsFee3F3tBjdy03LsFEm6HqKhpesdByVBd'
 URL_LATEST = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
 URL_HISTORICAL = f'https://api.freecurrencyapi.com/v1/historical?apikey={API_KEY}'
-CURRENCIES = ('USD', 'EUR', 'JPY', 'CAD')
+URL_CURRENCY_INFO = f'https://api.freecurrencyapi.com/v1/currencies?apikey={API_KEY}'
+CURRENCIES = ('USD', 'EUR', 'JPY', 'CAD', 'CHF', 'NZD')
+
 
 # Function to get the latest currency exchange rates
 def getLatestResults(currency_base):
     currencies = ','.join(CURRENCIES)
     url = f'{URL_LATEST}&base_currency={currency_base}&currencies={currencies}'
     result = requests.get(url).json()['data']
-    print(result)
     if currency_base in result: del result[currency_base]
     return result
+
 
 # Function to get the historical currency exchange rates
 def getHistoricalResults(currency_base, date):
@@ -24,20 +26,44 @@ def getHistoricalResults(currency_base, date):
     if currency_base in result: del result[currency_base]
     return result
 
+
+def getCurrencyAdditionalInfo():
+    currencies = ','.join(CURRENCIES)
+    url = f'{URL_CURRENCY_INFO}&currencies={currencies}'
+    result = requests.get(url).json()['data']
+    return result
+
+
+def printCurrencies(dictionary_of_currencies):
+    additional_info = getCurrencyAdditionalInfo()
+    for carrency_name, value in dictionary_of_currencies.items():
+        print(
+            f"{carrency_name}: {value: .4f}, {additional_info[carrency_name]['symbol']}, {additional_info[carrency_name]['name']}")
+
+
 if __name__ == '__main__':
     while True:
-        currency_base = input('What currency do you want to change?: ') # Currency for exchange
-        date = input('In what date are you interested in? (yyyy-mm-dd): ')  # Date of exchange
+        currency_base = input(
+            "What currency do you want to change? (press 'q' to quit): ").upper()  # Currency for exchange
 
-        # Try/except block for handling errors
-        try:
-            # if date is TODAY, then get the latest results of exchange rates
-            if date == datetime.today().strftime('%Y-%m-%d'):
-                print(f"Here's your TODAY data: {getLatestResults(currency_base)}")
-            # if date is NOT TODAY, then get the historical results of exchange rates
-            else:
-                print(f"Here's your HISTORICAL data: {getHistoricalResults(currency_base, date)}")
-        # If any error occurs, ask a user to try again and start the program from the beginning
-        except:
-            print("I can't load you any data with provided parameters. Try again!")
-            continue
+        if currency_base == 'Q':
+            exit()
+        else:
+            date = input('In what date are you interested in? (yyyy-mm-dd): ')  # Date of exchange
+
+            # Try/except block for handling errors
+            try:
+                # if date is TODAY, then get the latest results of exchange rates
+                if date == datetime.today().strftime('%Y-%m-%d'):
+                    latest_results = getLatestResults(currency_base)
+                    print("Here's your TODAY data:")
+                    printCurrencies(latest_results)
+                # if date is NOT TODAY, then get the historical results of exchange rates
+                else:
+                    historical_results = getHistoricalResults(currency_base, date)
+                    print("Here's your HISTORICAL data:")
+                    printCurrencies(historical_results)
+            # If any error occurs, ask a user to try again and start the program from the beginning
+            except:
+                print("I can't load you any data with provided parameters. Try again!")
+                continue
